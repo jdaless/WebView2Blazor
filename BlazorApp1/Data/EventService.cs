d19497b76ce69d069fc244f5808081b076b9a913
John D'Alessandro <jdalessandro@KLHENGRS.COM>
2020-12-15 15:15:11 -0500

    Add project files.

---
+++ BlazorApp1/Data/EventService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class EventService
+    {
+        public event EventHandler Event;
+
+        public void TriggerEvent() => Event.Invoke(this, null);
+
+        public int Handled = 0;
+    }
+}
